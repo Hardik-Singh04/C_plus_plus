2f51772fa07abe2674fb1ae584f064d655bde717
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-10 19:20:26 +0530

    Create code.cpp

---
+++ Divide two numbers/Normal/code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,divi=0;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    divi=a/b;
+    cout<<"Division is :"<<divi;
+    return 0;
+}
