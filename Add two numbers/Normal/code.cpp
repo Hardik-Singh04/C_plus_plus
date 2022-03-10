2e921778bc6d054be1a9295d71a2967bfbbf1b6f
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-10 19:12:41 +0530

    Create code.cpp

---
+++ Add two numbers/Normal/code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum=0;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"Sum is :"<<sum;
+    return 0;
+}
