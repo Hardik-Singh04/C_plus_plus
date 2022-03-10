da4fd3c70b21f1d545bb62e00ff786e3a8e8161a
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-10 19:16:34 +0530

    Create code.cpp

---
+++ Multiply two numbers/Normal/code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,multi=0;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    multi=a*b;
+    cout<<"Multiplication is :"<<multi;
+    return 0;
+}
