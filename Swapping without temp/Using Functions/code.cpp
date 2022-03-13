ee8ddcb93fd0b1aecb226ac7866a24dc8c4ab7ad
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-13 21:58:17 +0530

    Create code.cpp

---
+++ Swapping without temp/Using Functions/code.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void swap(int a, int b)
+{
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    
+    cout<<"Value of a after swapping is :"<<a<<endl;
+    cout<<"Value of b after swapping is :"<<b<<endl;
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter three numbers :";
+    cin>>a>>b;
+
+    cout<<"Value of a before swapping is :"<<a<<endl;
+    cout<<"Value of b before swapping is :"<<b<<endl<<endl;
+
+    swap(a,b);
+
+    return 0;
+}
