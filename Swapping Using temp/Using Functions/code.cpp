2df1aac78f3f596ac3e00aa79fc6ece2f43618ee
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-13 21:59:36 +0530

    Update code.cpp

--- Swapping Using temp/Using Functions/code.cpp
+++ Swapping Using temp/Using Functions/code.cpp
@@ -1,1 +1,27 @@
-.
+#include<iostream>
+using namespace std;
+
+void swap(int a, int b)
+{
+    int temp;
+    
+    temp=a;
+    a=b;
+    b=temp;
+    
+    cout<<"Value of a after swapping is :"<<a<<endl;
+    cout<<"Value of b after swapping is :"<<b<<endl;
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter three numbers :";
+    cin>>a>>b;
+
+    cout<<"Value of a before swapping is :"<<a<<endl;
+    cout<<"Value of b before swapping is :"<<b<<endl<<endl;
+
+    swap(a,b);
+
+    return 0;
+}
