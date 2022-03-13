91e41267e5869304d35e894d5f086770036ce511
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-13 17:03:23 +0530

    Create code.cpp

---
+++ Largest of 3 numbers/Normal/code.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers :";
+    cin>>a>>b>>c;
+
+    if(a>b && b>c)
+    {
+        cout<<a<<" is largest."<<endl;
+    }
+    else if(b>c && c>a)
+    {
+        cout<<b<<" is largest."<<endl;
+    }
+    else
+    {
+        cout<<c<<" is largest."<<endl;
+    }
+
+    return 0;
+}
