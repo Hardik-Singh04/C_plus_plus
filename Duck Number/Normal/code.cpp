e9472afadd6e944222d9e9c56394c209714c463c
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-14 18:59:21 +0530

    Create code.cpp

---
+++ Duck Number/Normal/code.cpp
@@ -0,0 +1,28 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num,temp,r,flg;
+	flg=0;
+ cout << " Input a number: ";
+ cin >> num;
+ temp=num;
+    while(num>0)
+        {
+            if(num % 10 == 0)
+            {
+            flg=1;
+            break;
+            }
+			num/=10;
+        }
+            if(temp>0 && flg==1)
+            {
+            cout << "Duck Number."<<endl;
+            }
+            else
+            {
+            cout << " Not a Duck Number."<<endl;
+            }
+}
