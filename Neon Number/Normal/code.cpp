6d2e4f3a77a88daf0a8c78a63aabed2a10d71dae
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-14 19:33:43 +0530

    Update code.cpp

--- Neon Number/Normal/code.cpp
+++ Neon Number/Normal/code.cpp
@@ -1,1 +1,31 @@
-.
+# include <bits/stdc++.h>
+using namespace std;
+
+void find(int num)
+{
+    
+}
+int main()
+{
+    int num,rem=0,sq,sum=0;
+	
+    cout << " Input a number: ";
+    cin >> num;
+   
+    sq=num*num;
+
+    while(sq>0)
+    {
+        rem=sq%10;
+        sum=sum+rem;
+        sq=sq/10;
+    }
+    if(sum==num)
+    {
+        cout << "Neon Number."<<endl;
+    }
+    else
+    {
+        cout << " Not a Neon Number."<<endl;
+    }
+}
