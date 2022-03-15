4a79306a1084ee69b4c3e4924f983a3839638290
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-15 19:15:53 +0530

    Update code.cpp

--- Switch Case Basic Questions/Area of Solids/Normal/code.cpp
+++ Switch Case Basic Questions/Area of Solids/Normal/code.cpp
@@ -1,1 +1,30 @@
-.
+#include <iostream>
+using namespace std;
+int main()
+{
+        int ch,a=0,b=0;
+        cout<<"1.Circle"<<endl;
+        cout<<"2.Rectangle"<<endl;
+        cout<<"3.Square"<<endl;
+        cout<<"4.Exit"<<endl;
+        cout<<"Enter your choice :";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:cout<<"Enter radius of circle :";
+                cin>>a;
+               cout<<"Area of circle : "<<(a*a*3.14);
+                break;
+            case 2:cout<<"Enter length of rectangle :";
+                cout<<"Enter breadth of length :";
+                cin>>a;
+                cin>>b;
+                cout<<"Area of rectangle: "<<(a*b);
+                break;
+            case 3:cout<<"Enter sides of square :";
+                cin>>a;
+                cout<<"Area of Square : "<<(a*a);
+                break;
+            default :cout<<"Wrong Choice.";
+        }
+}
