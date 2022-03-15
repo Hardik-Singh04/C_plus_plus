3f9dc0e07c2086a96851cd8ae616ce51c3569a55
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-15 19:35:05 +0530

    Update code.cpp

--- Switch Case Basic Questions/Perforn Mathematical/Normal/code.cpp
+++ Switch Case Basic Questions/Perforn Mathematical/Normal/code.cpp
@@ -1,1 +1,46 @@
-.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        int ch,a=0,b=0;
+        cout<<"Enter your choice :";
+        cin>>ch;
+
+        switch(ch)
+        {
+            case 1:
+                cout<<"Enter 2 numbers :"<<endl;
+                cin>>a>>b;
+                cout<<"Add :"<<(a+b);
+                break;
+            case 2:
+                cout<<"Enter 2 numbers :"<<endl;
+                cin>>a>>b;
+                if(a>b)
+                {
+                    cout<<"Subtract (a-b) :"<<(a-b);
+                }
+                else
+                {
+                    cout<<"Subtract (b-a) :"<<(b-a);
+                }
+                break;
+            case 3:
+                cout<<"Enter 2 numbers :"<<endl;
+                cin>>a>>b;
+                cout<<"Multiply :"<<(a*b);
+                break;
+            case 4:
+                cout<<"Enter 2 numbers :"<<endl;
+                cin>>a>>b;
+                cout<<"Divide :"<<(a/b);
+                break;
+            case 5:
+                cout<<"Enter 2 numbers :"<<endl;
+                cin>>a>>b;
+                cout<<"Modulus :"<<(a%b);
+                break;
+            default :cout<<"Wrong Choice.";
+        }
+}
