020837ec2d2c1a6adba24fb849bf0b5ad8a9cb1d
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2023-04-08 12:11:48 +0530

    Create Sum&Average.cpp

---
+++ Arrays/Sum&Average.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    long long int sum=0;
+    cout<<"Enter the number of elements you want to enter in an array : ";
+    cin>>n;
+    long long int arr[n];
+    float average=0;
+    for(int i =0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    average=sum/(float)n;
+    cout<<"The sum of an element in an array is = "<<sum<<endl;
+    cout<<"The average of an element in an array is = "<<average<<endl;
+    return 0;
+}
